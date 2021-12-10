8bc8aedd7576ad6fae78d6badde60e5fd421967c
ahmadfma <boboykecil257@gmail.com>
2021-12-10 11:10:57 +0800

    initial commit

---
+++ app/src/main/java/com/hafiz/tugas_final_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hafiz.tugas_final_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
