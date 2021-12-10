8a23562fdba06bad5127b268a0a7db5a0dcf7aa0
ahmadfma <boboykecil257@gmail.com>
2021-12-10 11:45:49 +0800

    buat class NewsAdapter

--- app/src/main/java/com/hafiz/tugas_final_android/UI/List/ListViewModel.kt
+++ app/src/main/java/com/hafiz/tugas_final_android/UI/List/ListViewModel.kt
@@ -2,6 +2,7 @@ package com.hafiz.tugas_final_android.UI.List
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.hafiz.tugas_final_android.adapter.NewsAdapter
 import com.hafiz.tugas_final_android.model.Article
 import com.hafiz.tugas_final_android.model.DataNewsItem
 import com.hafiz.tugas_final_android.network.RetrofitClient
@@ -11,14 +12,13 @@ import java.lang.Exception
 class ListViewModel: ViewModel() {
     private var TAG = "ListViewModel"
 
-    private var listNews = mutableListOf<Article>()
 
-    suspend fun getLatesNews() {
+    suspend fun getLatestNews(adapter: NewsAdapter) {
         var response: Response<DataNewsItem>? = null
         try {
             response = RetrofitClient.instance.getLatestNews()
             if(response.isSuccessful) {
-                listNews.addAll(response.body()?.articles as MutableList<Article>)
+                adapter.setData(response!!.body()!!.articles as MutableList<Article>)
             }
         } catch (e: Exception) {
             Log.e(TAG, "${e.message}")
---
+++ app/src/main/java/com/hafiz/tugas_final_android/adapter/NewsAdapter.kt
@@ -0,0 +1,48 @@
+package com.hafiz.tugas_final_android.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hafiz.tugas_final_android.R
+import com.hafiz.tugas_final_android.model.Article
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_layout_list_news.view.*
+
+class NewsAdapter(): RecyclerView.Adapter<NewsAdapter.Holder>() {
+
+    private var listNews = mutableListOf<Article>()
+
+    inner class Holder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(article: Article) {
+            with(itemView) {
+                Picasso.get()
+                    .load(article.urlToImage.toString())
+                    .into(thumbnailNews)
+
+                titleNews.text = article.title
+                publisherNews.text = article.source.name
+            }
+        }
+    }
+
+    fun setData(list: MutableList<Article>) {
+        this.listNews = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_list_news, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(listNews[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listNews.size
+    }
+
+
+}
